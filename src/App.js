42b84512d4a227310c2640fafe822057b65beba1
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-09 20:45:31 -0400

    header, header image complete. about me section added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,23 @@ import Header from './Header'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="main">
         <Header />
+        <section>
+          <img className="headshot" src="/images/headshot.jpg" alt="headshot" />
+        </section>
+        <div>
+          <div className="about-me">
+            <h1>About Me</h1>
+            <p>
+              Spicy jalapeno bacon ipsum dolor amet boudin shank meatball ham
+              hock picanha pastrami. Cupim sirloin buffalo swine ham hock
+              bresaola. Venison kielbasa hamburger shoulder beef ribs tri-tip
+              fatback leberkas. Prosciutto cow rump bresaola burgdoggen.
+              Leberkas doner tongue salami alcatra pastrami b
+            </p>
+          </div>
+        </div>
       </div>
     )
   }
