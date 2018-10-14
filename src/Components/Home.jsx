bee888738320dea80b66022342d3330f5d9498ee
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-13 22:40:12 -0400

    checkpoint

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,14 +3,12 @@ import React, { Component } from 'react'
 import Header from './Header'
 import Headshot from './Headshot'
 import AboutMe from './AboutMe'
-import Menu from './Menu'
 
 class Home extends Component {
   render() {
     return (
       <>
         <Header />
-        {/* <Menu /> */}
         <Headshot />
         <AboutMe />
       </>
