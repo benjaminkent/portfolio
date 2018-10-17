17d8ce5db5d2a4d1bd7031a0686c73bc44ea1af7
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-10-17 12:02:16 -0400

    updated alt logo in language

--- src/Components/Languages.jsx
+++ src/Components/Languages.jsx
@@ -32,7 +32,7 @@ class Languages extends Component {
             />
             <img className="language" alt="HTML logo" src="./images/html.png" />
             <img className="language" alt="CSS logo" src="./images/css.png" />
-            <img className="language" alt="CSS logo" src="./images/sass.png" />
+            <img className="language" alt="SCSS logo" src="./images/sass.png" />
           </div>
           <p className="skills-lang">
             In order: React, Ruby, Rails, Phaser, JavaScript, HTML5, CSS3 and
