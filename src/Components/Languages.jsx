257a66890611960f0d0d6c2032830b52bfda8a3e
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-11-08 10:21:29 -0500

    working on language flip

--- src/Components/Languages.jsx
+++ src/Components/Languages.jsx
@@ -8,37 +8,75 @@ class Languages extends Component {
           <p className="skills">
             <strong>My current tool-set includes:</strong>
           </p>
-          <div className="language-container">
-            <img
-              className="language"
-              alt="React logo"
-              src="./images/react.png"
-            />
-            <img className="language" alt="Ruby logo" src="./images/ruby.png" />
-            <img
-              className="language"
-              alt="Rails logo"
-              src="./images/rails.png"
-            />
-            <img
-              className="language"
-              alt="PostgreSQL logo"
-              src="./images/postgresql-logo.png"
-            />
-            <img
-              className="language"
-              alt="JavaScript logo"
-              src="./images/javascript.png"
-            />
-            <img className="language" alt="SCSS logo" src="./images/sass.png" />
-            <img className="language" alt="HTML logo" src="./images/html.png" />
-            <img className="language" alt="CSS logo" src="./images/css.png" />
-            <img
-              className="language"
-              alt="Phaser logo"
-              src="./images/phaser.png"
-            />
-          </div>
+          <ul className="language-container">
+            <li
+              className="lang-image"
+              style={{
+                backgroundImage: 'url(/images/react.png)'
+              }}
+            >
+              {/* <img
+                className="language"
+                alt="React logo"
+                src="./images/react.png"
+              /> */}
+              <div className="lang-description">
+                <p>React.js</p>
+              </div>
+            </li>
+            <li>
+              <img
+                className="language"
+                alt="Ruby logo"
+                src="./images/ruby.png"
+              />
+            </li>
+            <li>
+              <img
+                className="language"
+                alt="Rails logo"
+                src="./images/rails.png"
+              />
+            </li>
+            <li>
+              <img
+                className="language"
+                alt="PostgreSQL logo"
+                src="./images/postgresql-logo.png"
+              />
+            </li>
+            <li>
+              <img
+                className="language"
+                alt="JavaScript logo"
+                src="./images/javascript.png"
+              />
+            </li>
+            <li>
+              <img
+                className="language"
+                alt="SCSS logo"
+                src="./images/sass.png"
+              />
+            </li>
+            <li>
+              <img
+                className="language"
+                alt="HTML logo"
+                src="./images/html.png"
+              />
+            </li>
+            <li>
+              <img className="language" alt="CSS logo" src="./images/css.png" />
+            </li>
+            <li>
+              <img
+                className="language"
+                alt="Phaser logo"
+                src="./images/phaser.png"
+              />
+            </li>
+          </ul>
           <p className="skills-lang">
             In order: React.js, Ruby, Rails, PostgreSQL, JavaScript, Sass,
             HTML5, CSS3 and Phaser
