cf09161dd9b08801bea9ea76d959ac2fba0f8b38
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-11-15 18:59:25 -0500

    changed to browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './Components/Home'
 import Portfolio from './Components/Portfolio'
